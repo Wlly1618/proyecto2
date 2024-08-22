f0eac7cae52e061062c70ab59ee38a1341db0a5e
Mili Antoni <miliantoni12@gmail.com>
2024-08-22 16:41:13 -0300

    terminada la seccion disney

--- scripts/main.js
+++ scripts/main.js
@@ -8,5 +8,19 @@ function scrollRight() {
     container.scrollBy({ left: 150, behavior: 'smooth' });
 } */
 
+    let scrollContainer = document.querySelector(".gallery");
+    let backBtn = document.getElementById('backBtn');
+    let nextBtn = document.getElementById('nextBtn');
   
-
+    scrollContainer.addEventListener("wheel", (e) => {
+      e.preventDefault();
+      scrollContainer.scrollLeft += e.deltaY;
+    });
+  
+    nextBtn.addEventListener('click', () => {
+      scrollContainer.scrollLeft += scrollContainer.clientWidth;
+    });
+  
+    backBtn.addEventListener('click', () => {
+      scrollContainer.scrollLeft -= scrollContainer.clientWidth;
+    });
